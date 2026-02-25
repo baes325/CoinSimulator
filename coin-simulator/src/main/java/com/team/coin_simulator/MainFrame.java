e8a7b1a86b288371f35e1ac7bd6d0ffdc2862423
BaeSujin <plerain0325@naver.com>
2026-02-25 10:02:09 +0900

    작업 내용 임시 저장

--- coin-simulator/src/main/java/com/team/coin_simulator/MainFrame.java
+++ coin-simulator/src/main/java/com/team/coin_simulator/MainFrame.java
@@ -337,10 +337,7 @@ public class MainFrame extends JFrame {
         super.dispose();
     }
 
-    // ════════════════════════════════════════════════
-    //  진입점
-    // ════════════════════════════════════════════════
-
+    //진입점
     public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
